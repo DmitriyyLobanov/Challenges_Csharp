56dfe942db2b96429471b370cb20ba8114324974
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-19 19:00:20 +0400

    first iteracion

---
+++ Friends_and_dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstFriendspeed = 1;
+int secondFriendspeed = 2;
+int dogSpeed = 5;
+int distance = 10000;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendspeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendspeed + dogSpeed);
+        friend = 1;
+    }
+    
+distance = distance - (firstFriendspeed + secondFriendspeed) * time;
+count++;
+
+}
+
+Console.WriteLine($"Собака пробежит {count} раз.");
